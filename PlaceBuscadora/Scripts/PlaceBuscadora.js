521f53c679e44eefa4816e5a24d6dc3c6c706fe0
Edgar Buenrostro <goodface87@gmail.com>
2013-02-28 21:51:09 -0800

    Adds a bit of animation.

--- PlaceBuscadora/Scripts/PlaceBuscadora.js
+++ PlaceBuscadora/Scripts/PlaceBuscadora.js
@@ -1,5 +1,7 @@
 ﻿$(document).ready(function () {
     $('.search-container input').keyup(function () {
-        $('.result').css("margin-top", "300px");
+        //$('.result').css("margin-top", "300px");
+        
+        $('.result').removeClass('result').addClass('result-found');
     });
 });
